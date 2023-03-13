c770324750007b088fa0e816a64b62591c61469b
Himal <himel@bdtask.net>
2023-03-13 18:01:13 +0600

    About page content and design

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
-    <div>This is About component</div>
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2> Introduction </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+      </p>
+      <h2>Service Offered</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+      </p>
+      <p>we offer the following services </p>
+      <u>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+        <li>Service 4</li>
+        <li>Service 5</li>
+        <li>Service 6</li>
+        <li>Service 7</li>
+      </u>
+      <h2>Contact Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dolorum omnis tempora minus consectetur quasi nemo praesentium,  
+      </p>
+    </div>
   )
 }
 
