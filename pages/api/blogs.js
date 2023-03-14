62bb965317eb8f8f668a91c892279240f16fb12c
Himal <himel@bdtask.net>
2023-03-14 19:09:34 +0600

    Add Infinite scroll

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,12 +2,17 @@
 import * as fs from 'node:fs';
 
 export default async function handler(req, res) {
-  // //Test fetch data from api page
   // fetch('https://api.agify.io?name=meelad')
   // .then((data) => data.json())
   // .then((data) => console.log(data));
 
+  
   let data = await fs.promises.readdir('./blogData');
+  let allCount = data.length;
+
+  data = data.slice(0, parseInt(req.query.count));
+ 
+
   let allBlogs = [];
   let myFile ;
   for (let index = 0; index < data.length; index++) {
@@ -15,8 +20,10 @@ export default async function handler(req, res) {
     myFile = await fs.promises.readFile((`./blogData/${item}`),'utf-8');
     allBlogs.push(JSON.parse(myFile));
   }
-  res.status(200).json(allBlogs);
-  
-  
-  
+  // res.status(200).json(allBlogs); 
+  res.status(200).json({
+    allBlogs,
+    allCount
+  }); 
+
 }
--- pages/blog.js
+++ pages/blog.js
@@ -3,14 +3,25 @@ import React, {useEffect, useState} from 'react'
 import styles from '../styles/Blog.module.css'
 import { Message_Value } from '../context/context'
 import Head from 'next/head'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 // Step 1: collect all the files from blog data directory
 // step 2: Iterate through the and display them
 
 const Blog = (props) => {
-  const { message, apiData} = Message_Value();
-  console.log(apiData);
-  const [blogs, setBlogs] = useState(props.allBlogs);
+    const { message, apiData} = Message_Value();
+    const [count, setCount] = useState(2);
+    const [blogs, setBlogs] = useState(props.allBlogs);
+
+    const fetchData = async () => {
+      // console.log(props.allCount);
+      let d = await fetch(`http://localhost:3000/api/blogs/?count=${count + 2}`);
+      setCount(count + 2);
+      const {allBlogs} = await d.json();
+      setBlogs(allBlogs);
+    };
+
 
   // useEffect(() => {
   //   console.log('Use Effect is runing');
@@ -24,18 +35,34 @@ const Blog = (props) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{message } - {blogs.length} </title>
+        <title>This is Blog </title>
       </Head>
       <main className={styles.main}>
-        { blogs.length > 0 && blogs.map((blog, index) => (
-          <div className="blogItem" key={index}>
-            <Link href={`/blogpost/${blog.slug}`}>
-              <h3 className={styles.blogItemH3}>{blog.title}</h3>
-            </Link>
-              <p>{ blog.content.substring(0, 100) }...</p>
-						<button className={styles.btn}> Read More</button>
-          </div>
-        ))}
+        <InfiniteScroll
+          // dataLength={props.allCount !== blogs.length} //This is important field to render the next data
+          dataLength={props.allCount}
+          next={fetchData}
+          hasMore={props.allCount != blogs.length}
+          loader={<h4>Loading...</h4>} 
+          endMessage={
+            <p style={{textAlign: 'center'}}>
+              <b>Yay! You have seem it all</b>
+            </p>
+          }
+        >
+          { blogs.length > 0 && blogs.map((blog, index) => {
+            console.log(props.allCount,  blogs.length, props.allCount != blogs.length, count);
+
+            return (
+            <div className="blogItem" key={index}>
+              <Link href={`/blogpost/${blog.slug}`}>
+                <h3 className={styles.blogItemH3}>{blog.title}</h3>
+              </Link>
+                <p>s ss { blog.content.substring(0, 100) }...</p>
+              <button className={styles.btn}> Read More</button>
+            </div>
+          )})}
+        </InfiniteScroll>
       </main>
     </div>
   )
@@ -43,12 +70,15 @@ const Blog = (props) => {
 
 
 export async function getServerSideProps(context){
+    // let data = await fs.promises.readdir('./blogData');
+    // let allCount = data.length;
+
     // Fetch data from external API
-    const res = await fetch(`http://localhost:3000/api/blogs`)
-    const allBlogs = await res.json()
+    const res = await fetch(`http://localhost:3000/api/blogs?count=2`)
+    const {allBlogs, allCount} = await res.json()
 
     // Pass data to the page via props
-    return { props: { allBlogs } }
+    return { props: { allBlogs, allCount }}
 }
 
 export default Blog
